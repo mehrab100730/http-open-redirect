0cfbd84943874b11a2fb588d46b22906e8853d69
demetriusx00 <71256266+demetriusx00@users.noreply.github.com>
2020-12-30 11:24:37 -0800

    Add main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import asyncio
+import click
+
+from core.utils import validate_url
+
+__author__ = 'Demetrius Ford'
+
+
+@click.command()
+@click.option('--url',
+              callback=validate_url,
+              required=True)
+@click.option('--cookie',
+              default=None)
+def cli(url, cookie):
+    """Test for open redirect."""
+
+
+if __name__ == '__main__':
+    cli()
