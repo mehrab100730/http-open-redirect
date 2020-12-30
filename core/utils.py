2bd2ed4c2184792d8e27fc90b018463dce1bc676
demetriusx00 <71256266+demetriusx00@users.noreply.github.com>
2020-12-30 10:13:51 -0800

    Add core/utils.py

---
+++ core/utils.py
@@ -0,0 +1,25 @@
+import urllib.parse
+
+from click import BadParameter
+from validators import url
+
+
+def validate_url(ctx, param, value):
+    if not url(value, public=False):
+        raise BadParameter('url is not valid.')
+    return value
+
+# => Credit sqlmap-dev (@sqlmapproject)
+# => https://github.com/sqlmapproject/sqlmap/blob/210a4c3a0a5dcaf984576e0597a79eb23fa9afa4/extra/vulnserver/vulnserver.py#L117-L122
+
+
+def convert_cookie(cookie):
+    if cookie is None:
+        return cookie
+
+    cookie_dict = {}
+    for token in cookie.split(';'):
+        token = token.strip()
+        if '=' in token:
+            key, value = token.split('=', 1)
+            cookie_dict[key.strip()] = urllib.parse.unquote_plus(value.strip())
