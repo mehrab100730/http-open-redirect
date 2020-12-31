e65945bfab3b600d6e5c47e697ea45f043d13ae3
demetriusx00 <71256266+demetriusx00@users.noreply.github.com>
2020-12-31 00:13:09 -0800

    Update added type hinting @ [line 9]

--- core/utils.py
+++ core/utils.py
@@ -1,10 +1,12 @@
 import urllib.parse
 
 from click import BadParameter
+from click.core import Context
+from click.core import Option
 from validators import url
 
 
-def validate_url(ctx, param, value):
+def validate_url(ctx: Context, param: Option, value: str):
     if not url(value, public=False):
         raise BadParameter('url is not valid.')
     return value
@@ -13,7 +15,7 @@ def validate_url(ctx, param, value):
 # => https://github.com/sqlmapproject/sqlmap/blob/210a4c3a0a5dcaf984576e0597a79eb23fa9afa4/extra/vulnserver/vulnserver.py#L117-L122
 
 
-def convert_cookie(cookie):
+def convert_cookie(cookie: str):
     if cookie is None:
         return cookie
 
