71776433817b1122047a158e00b733e8452882e1
demetriusx00 <71256266+demetriusx00@users.noreply.github.com>
2020-12-30 19:06:35 -0800

    Update added new flag: "--workers"

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import asyncio
+import time
 import click
+import multiprocessing
 
 from core.utils import validate_url
+from core.scanner import scan
 
 __author__ = 'Demetrius Ford'
 
@@ -12,8 +15,15 @@ __author__ = 'Demetrius Ford'
               required=True)
 @click.option('--cookie',
               default=None)
-def main(url, cookie):
+@click.option('--workers',
+              default=multiprocessing.cpu_count())
+def main(url, cookie, workers):
     """Test for open redirect."""
+    started = time.perf_counter()
+    asyncio.run(scan(url, cookie, workers))
+    elapsed = time.perf_counter() - started
+    
+    click.echo(f'🎉 completed in {elapsed:0.2f} seconds.')
 
 
 if __name__ == '__main__':
