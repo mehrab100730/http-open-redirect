824503f3d846169131f2f0e2454b5353a905e5ab
demetriusx00 <71256266+demetriusx00@users.noreply.github.com>
2020-12-30 18:59:41 -0800

    Add core/scanner.py

---
+++ core/scanner.py
@@ -0,0 +1,38 @@
+import click
+import pypeln as pl
+import urllib.parse
+
+from aiohttp import ClientSession
+from aiohttp import TCPConnector
+
+from core.urls import make_urls
+from core.utils import convert_cookie
+
+# => Target redirect
+
+HACKER_ONE = 'https://www.hackerone.com'
+
+# => Known redirects
+
+HTTP_CODES = (301,
+              302,
+              303,
+              307,
+              308)
+
+
+async def scan(url: str, cookie: str, workers: int):
+    async with ClientSession(connector=TCPConnector(limit=0),
+                             cookies=convert_cookie(cookie)) as session:
+        async def find_open_redirect(url):
+            async with session.get(url, allow_redirects=True) as response:
+                redirected = any(redirect.status in HTTP_CODES for
+                                 redirect in response.history)
+                vulnerable = redirected and \
+                    response.url.human_repr() == HACKER_ONE
+                if vulnerable:
+                    click.echo(f'😈 {urllib.parse.unquote_plus(url)}')
+
+        await pl.task.each(find_open_redirect,
+                           make_urls(url),
+                           workers=workers)
