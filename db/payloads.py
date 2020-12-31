1f6ae2ace1f6bfd4905777e1ac69d5e017340a2f
demetriusx00 <71256266+demetriusx00@users.noreply.github.com>
2020-12-31 10:42:15 -0800

    Add db/payloads.py

---
+++ db/payloads.py
@@ -0,0 +1,15 @@
+# Credit FuzzDB (@fuzzdb-project):
+# https://github.com/fuzzdb-project/fuzzdb/blob/master/attack/redirect/redirect-injection-template.txt
+
+OPEN_REDIRECT_PAYLOADS = ['hackerone.com',
+                          '/hackerone.com',
+                          '//hackerone.com',
+                          '///hackerone.com',
+                          '////hackerone.com',
+                          '/http://hackerone.com',
+                          '/https://hackerone.com',
+                          '%2fhackerone.com',
+                          '%2f$2fhackerone.com',
+                          '%2fhackerone.com%2f%2f',
+                          '$2f%2fhackerone.com%2f%2f',
+                          '%2fhackerone.com//',]
