d19bc280cdfc94601f4787f590f41e979f6bee31
demetriusx00 <71256266+demetriusx00@users.noreply.github.com>
2020-12-30 18:58:21 -0800

    Update removed unused dependencies
    In addition, I changed the return type to just yield a string. This was to prevent any underlying complexity within the pypeln module. If at any point I need the payload, it's a simple change to implement.

--- core/urls.py
+++ core/urls.py
@@ -2,14 +2,12 @@ import re
 import urllib.parse
 
 from typing import Iterator
-from typing import Tuple
-from typing import TypeVar
 
 from core.dorks import OPEN_REDIRECT_DORKS
 from core.payloads import OPEN_REDIRECT_PAYLOADS
 
 
-def make_urls(url: str) -> Iterator[Tuple[str, str]]:
+def make_urls(url: str) -> Iterator[str]:
     if url[-1] == '/':
         url = url[:-1]
 
@@ -22,13 +20,10 @@ def make_urls(url: str) -> Iterator[Tuple[str, str]]:
                 for payload in OPEN_REDIRECT_PAYLOADS:
                     param_value = re.search(rf'{dork}=([^&]+)', url)
                     if param_value is not None:
-                        yield (url.replace(
-                               param_value.group(1),
-                               payload),
-                               payload)
+                        yield url.replace(param_value.group(1), payload)
     else:
         for payload in OPEN_REDIRECT_PAYLOADS[1:]:
             if payload[0] != '/':
-                yield (f'{url}/{payload}', payload)
+                yield f'{url}/{payload}'
             else:
-                yield ((url + payload), payload)
+                yield url + payload
